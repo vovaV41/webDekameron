25b80f2a43ae88cd1527d542b13eb128dbd3fbb7
root <start>
2024-07-13 19:37:36 +0300

    преметівний інтерфейс без логіки

--- logika.js
+++ logika.js
@@ -1,19 +1,46 @@
 
 window.onload = function(){
 
-    addElement("Text", "t1", "Текст 1", "elements-container");
-    addElement("Button", "b1", "Вибор 1", "elements-container");
+    addElement("Text", "t0", "Текст 1", "elements-container");
+    addElement("Button", "b0", "Вибор 1", "elements-container");
 
-    addElement("Text", "t2", "Текст 2", "elements-container");
-    addElement("Button", "b2", "Вибор 2", "elements-container");
+    addElement("Text", "t1", "Текст 2", "elements-container");
+    addElement("Button", "b1", "Вибор 2", "elements-container");
 
-    addElement("Text", "t3", "Текст 3", "elements-container"); 
-    addElement("Button", "b3", "Вибор 3", "elements-container");
+    addElement("Text", "t2", "Текст 3", "elements-container"); 
+    addElement("Button", "b2", "Вибор 3", "elements-container");
+
+setImage("background.bmp");
     
 addText( "Захочеш найдеш!!!\n 55555");
+document.getElementById('b0').addEventListener('click', function() {
+    onClick('b0');
+});
+
+document.getElementById('b1').addEventListener('click', function() {
+    onClick('b1');
+});
+
+document.getElementById('b2').addEventListener('click', function() {
+   onClick('b2');
+});
+
 }
 
+function onClick(id){
+    document.getElementById('b0').setAttribute("class","falling-button");
+    document.getElementById('b1').setAttribute("class","falling-button");
+    document.getElementById('b2').setAttribute("class","falling-button");
 
+}
+
+function setImage(patch){
+    var image = document.getElementById("image");
+    image.style.backgroundImage="url('"+patch+"')";
+    //image.style.display= "none";
+  
+    
+}
 
 
 function addElement( element, id, text, perent) {
